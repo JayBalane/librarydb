ce2ead03f4a827efa83450c7e7ae6635a894f974
Sleepy-J <jaybalanee@gmail.com>
2025-05-21 22:41:34 +0800

    views added

---
+++ librarydb/views.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace librarydb
+{
+    partial class views
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.logout_button = new System.Windows.Forms.Button();
+            this.overdue_books = new System.Windows.Forms.Button();
+            this.borrowers_list = new System.Windows.Forms.Button();
+            this.available_books = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.logout_button);
+            this.panel1.Controls.Add(this.overdue_books);
+            this.panel1.Controls.Add(this.borrowers_list);
+            this.panel1.Controls.Add(this.available_books);
+            this.panel1.Location = new System.Drawing.Point(-2, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(248, 451);
+            this.panel1.TabIndex = 3;
+            // 
+            // logout_button
+            // 
+            this.logout_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logout_button.Location = new System.Drawing.Point(75, 392);
+            this.logout_button.Name = "logout_button";
+            this.logout_button.Size = new System.Drawing.Size(94, 33);
+            this.logout_button.TabIndex = 6;
+            this.logout_button.Text = "Logout";
+            this.logout_button.UseVisualStyleBackColor = true;
+            // 
+            // overdue_books
+            // 
+            this.overdue_books.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.overdue_books.Location = new System.Drawing.Point(48, 197);
+            this.overdue_books.Name = "overdue_books";
+            this.overdue_books.Size = new System.Drawing.Size(155, 39);
+            this.overdue_books.TabIndex = 3;
+            this.overdue_books.Text = "Overdue Books";
+            this.overdue_books.UseVisualStyleBackColor = true;
+            // 
+            // borrowers_list
+            // 
+            this.borrowers_list.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.borrowers_list.Location = new System.Drawing.Point(48, 152);
+            this.borrowers_list.Name = "borrowers_list";
+            this.borrowers_list.Size = new System.Drawing.Size(155, 39);
+            this.borrowers_list.TabIndex = 2;
+            this.borrowers_list.Text = "Borrowers List";
+            this.borrowers_list.UseVisualStyleBackColor = true;
+            // 
+            // available_books
+            // 
+            this.available_books.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.available_books.Location = new System.Drawing.Point(48, 107);
+            this.available_books.Name = "available_books";
+            this.available_books.Size = new System.Drawing.Size(155, 39);
+            this.available_books.TabIndex = 1;
+            this.available_books.Text = "Available Books";
+            this.available_books.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Library \r\nManagement";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(252, 9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(536, 424);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // views
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 445);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "views";
+            this.Text = "views";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button logout_button;
+        private System.Windows.Forms.Button overdue_books;
+        private System.Windows.Forms.Button borrowers_list;
+        private System.Windows.Forms.Button available_books;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
